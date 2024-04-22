005369ff287075c72ffe9b06d6dec220628167cf
yaugesha <eugene.korchemenko@gmail.com>
2024-04-22 18:54:18 +0300

    feat: add header for course pages and table sort section in schedule

--- src/scripts/schedule.js
+++ src/scripts/schedule.js
@@ -1,2 +1,4 @@
 import "../styles/style.scss";
+import "../styles/layout/course-navigation.scss";
+import "../styles/pages/schedule/schedule-table-sort.scss";
 import "../styles/pages/schedule/schedule.scss";
