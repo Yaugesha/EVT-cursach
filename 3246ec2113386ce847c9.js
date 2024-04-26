e0eff6f1e446330d45d2e56ebce11fb95bbbf035
Yaugesha <eugene.korchemenko@gmail.com>
2024-04-26 12:31:57 +0300

    Updates

---
+++ 3246ec2113386ce847c9.js
@@ -0,0 +1,3 @@
+import "../styles/style.scss";
+import "../styles/layout/course-navigation.scss";
+import "../styles/pages/dashboard/dashboard.scss";
---
+++ be73a7fc550e850853fc.js
@@ -0,0 +1,12 @@
+import "../styles/style.scss";
+import "../styles/pages/course/course.scss";
+
+import { dropdownButton } from "./dropdownButton";
+import { printCode } from "./liveTextPrinting";
+import { burgerMenu } from "./burgerMenu";
+import { addPopupEventListener } from "./popup";
+
+dropdownButton();
+printCode();
+burgerMenu();
+addPopupEventListener();
