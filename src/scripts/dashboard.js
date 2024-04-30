b9ecd868a7f85b4eebde43e595b543e4d482874e
yaugesha <eugene.korchemenko@gmail.com>
2024-04-30 23:15:19 +0300

    feat: add responsive layout for course and dashboard pages

--- src/scripts/dashboard.js
+++ src/scripts/dashboard.js
@@ -1,3 +1,7 @@
 import "../styles/style.scss";
 import "../styles/layout/course-navigation.scss";
 import "../styles/pages/dashboard/dashboard.scss";
+
+import { burgerMenu } from "./burgerMenu";
+
+burgerMenu();
